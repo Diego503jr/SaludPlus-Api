f233dbc7ed3cd59da8ff6c5a3b1a04162a75291c
Diego Carias <diegocarias789@gmail.com>
2026-05-21 22:59:17 -0600

    ADD: sopoorte para la url de la db

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,28 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 //Conexion directa a la base de datos por medio de variables de entorno
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false },
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        ssl: { rejectUnauthorized: false },
+      },
+);
+
+pool.connect((err) => {
+  if (err) {
+    console.error("Error conectando a la BD:", err.message);
+  } else {
+    console.log("Conectado a PostgreSQL correctamente");
+  }
 });
+
 module.exports = pool;
